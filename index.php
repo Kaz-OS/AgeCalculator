f578e75bb064125aec4d4bf4c41feea98f2a84ce
Kaz-OS <estebanrattier27@gmail.com>
2025-03-02 22:27:48 +0100

    Fist commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculateur d'age</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="age">
+        <div class="Input">
+            <label>
+                DAY
+                <input type="number">
+            </label>
+            <label>
+                MONTH
+                <input type="number">
+            </label>
+            <label>
+                YEAR
+                <input type="number">
+            </label>
+        </div>
+    </div>
+</body>
+</html>
